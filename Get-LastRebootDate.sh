078d632d294b3ecf65b51f7456cfe55dae47e0a2
stanislawhorna1 <stanislawhorna@outlook.com>
2023-05-30 23:05:29 +0200

    modified:   Get-LastRebootDate.sh

--- Get-LastRebootDate.sh
+++ Get-LastRebootDate.sh
@@ -23,12 +23,15 @@ Main() {
 }
 
 GetTimeSinceLastBoot() {
+    # grep -e to select line with Keyword "Time since boot:"
     var="$(System_profiler SPSoftwareDataType | grep -e "Time since boot:")"
     timeframe=${var#*: }
 }
 
 ParseStringTimeSpan() {
+    # sed 's/[^0-9]/ /g' to select digits only
     numbers=$(echo "$timeframe" | sed 's/[^0-9]/ /g')
+    # Split string FileVaultString by separator set in IFS and next $3 is a 3rd element in splitted string
     set -- $numbers
     days=$1
     hours=$2
@@ -36,12 +39,16 @@ ParseStringTimeSpan() {
 }
 
 CalculateSecondsSinceLastReboot() {
+    # $(( )) to calculate math expresion
     TotalSeconds=$(((days * 24 * 60 * 60) + (hours * 60 * 60) + (minutes * 60)))
+    # date +%s to return date in seconds since January 1, 1970
     CurrentDate=$(date +%s)
 }
 
 CalculateDateOfLastBoot() {
+    # $(( )) to calculate math expresion
     BootDateSeconds=$((CurrentDate - TotalSeconds))
+    # date -r to convert date from seconds since January 1, 1970 to standard format
     BootDate=$(date -r $BootDateSeconds)
 }
 

6961d064169c78e83b316e1c00f9493c196d90d3
stanislawhorna1 <stanislawhorna@outlook.com>
2023-06-12 22:58:01 +0200

    modified:   Measure-NetworkSpeed.sh

--- Measure-NetworkSpeed.sh
+++ Measure-NetworkSpeed.sh
@@ -1,22 +1,71 @@
 #!/bin/sh
 
+### DESCRIPTION
+# Script to measure network bandwith.
+# Speedtest is done using Speedtest® by Ookla® (https://www.speedtest.net/pl/apps/cli)
+
+### INPUTS
+# 1. DEBUG - false to prevent displaing results in console
+
+### OUTPUTS
+# Month
+# Timestamp                                    
+# ISP
+# Local Gateway IP
+# server name
+# idle latency [ms]
+# idle jitter [ms]
+# packet loss [%]
+# download [Mbps]
+# download latency [ms]
+# upload [Mbps]
+# upload latency [ms]
+# download bytes
+# upload bytes
+# share url
+
+### CHANGE LOG
+# Author:   Stanislaw Horna
+# Created:  12-Jun-2023
+# Version:  1.0
+
 DEBUG=$1
+OutputFile=$2
+
+PackageName="speedtest"
 
 Main() {
-    csv_file="./data.csv"
+    # Check if Speedtest® by Ookla® is installed
+    TestPackageInstalled
+    # Disable output to prevent displaying traceroute and speedtest output
+    DisableConsoleOutput
+    # Check which WAN gateway is in use
     GetISP
+    # Perform speedtest
     GetSpeedTest
+    # Enable output
+    EnableConsoleOutput
+    # Gather output from speedtest and convert to Mbps
     FormatSpeedTestOutput
+    # Get timestamp of the measurement
     GetTimeStamp
+    # Export results to CSV file
     ExportCSV
     if [ "$DEBUG" != "false" ]; then
-        echo "Upload: $Upload"
-        echo "Download: $Download"
-        echo "Latency: $Latency"
-        echo "Responsivness Up: $UplinkResponsivness"
-        echo "Responsivness Down: $DownLinkResponsivness"
-        echo "Gateway: $GatewayIP"
         echo "ISP provider: $ISPProvider"
+        echo "Server: $DestinationServer"
+        echo "Latency: $Latency ms"
+        echo "Download: $Download Mbps"
+        echo "Upload: $Upload Mbps"
+        echo "Packet Loss: $PacketLoss %"
+        echo "Link to test: $URL"
+    fi
+}
+
+TestPackageInstalled() {
+    if [ "$(brew list | grep $PackageName)" != $PackageName ]; then
+        echo "Cannot run the $PackageName, because it is not installed"
+        exit 1
     fi
 }
 
@@ -37,71 +86,104 @@ GetISP() {
 }
 
 GetInterfaceToMeasure() {
-
     WiFiInterfaceID=$(networksetup -listallhardwareports | awk '/Hardware Port: Wi-Fi/{getline; print $2}')
     InterfaceList=$(networksetup -listallhardwareports | awk '/Hardware Port:*/{getline; print $2}')
     NICtoMeasure="$WiFiInterfaceID"
     printf '%s\n' "$InterfaceList" | while IFS= read -r NIC; do
-    temp=$(ipconfig getifaddr $NIC)
+        temp=$(ipconfig getifaddr $NIC)
         if [ -n "$temp" ] && [ "$NIC" != "$WiFiInterfaceID" ]; then
             NICtoMeasure=$NIC
-            echo "$NIC IP address is not empty"
-            echo "IP: $temp"
         fi
     done
-    echo "NIC: $NICtoMeasure will be used to measure"
 }
 
 GetSpeedTest() {
-
-    SpeedTest=$(networkQuality -s)
-    IFS=$'\n'
-    set -- $SpeedTest
-    Upload=$2
-    Download=$3
-    UplinkResponsivness=$4
-    DownLinkResponsivness=$5
-    Latency=$6
-    #echo "$SpeedTest"
-    SpeedTest=$(echo "$SpeedTest" | tail -n 5)
-    SpeedTest=$(echo "$SpeedTest" | tr '\n' ' ')
-    #echo "$SpeedTest"
-    #SpeedTest=${SpeedTest//'\n'/' '}
+    SpeedTest=$(speedtest -I "$NICtoMeasure" -f csv)
 }
 
 FormatSpeedTestOutput() {
-    Upload=${Upload#*: }
-    Download=${Download#*: }
-    UplinkResponsivness=${UplinkResponsivness#*: }
-    DownLinkResponsivness=${DownLinkResponsivness#*: }
-    Latency=${Latency#*: }
-    IFS=$' '
-    set -- $Latency
-    Latency=$1
-    set -- $Upload
-    Upload=$1
-    set -- $Download
-    Download=$1
+    ## Select needed columns according to the split below
+    # 1 - "server name"
+    # 2 - "server id"
+    # 3 - "idle latency"
+    # 4 - "idle jitter"
+    # 5 - "packet loss"
+    # 6 - "download"
+    # 7 - "upload"
+    # 8 - "download bytes"
+    # 9 - "upload bytes"
+    # 10 - "share url"
+    # 11 - "download server count"
+    # 12 - "download latency"
+    # 13 - "download latency jitter"
+    # 14 - "download latency low"
+    # 15 - "download latency high"
+    # 16 - "upload latency"
+    # 17 - "upload latency jitter"
+    # 18 - "upload latency low"
+    # 19 - "upload latency high"
+    # 20 - "idle latency low"
+    # 21 - "idle latency high"
+    IFS=$','
+    set -- $SpeedTest
+    DestinationServer=$1
+    Latency=$3
+    PacketLoss=$5
+    Download=$6
+    Upload=$7
+    URL=${10}
+    Result_Speedtest="$1,$3,$4,$5,$6,${12},$7,${16},$8,$9,${10}"
+    # Remove quotes from number
+    Download="${Download#?}"
+    Download="${Download%?}"
+    # Calculate and round number
+    ## WARNING If you go to share URL they use 1000 as a divider, not 1024
+    Download=$((Download * 8 / 1000 / 1000))
+    # Remove quotes from number
+    Upload="${Upload#?}"
+    Upload="${Upload%?}"
+    # Calculate and round number
+    ## WARNING If you go to share URL they use 1000 as a divider, not 1024
+    Upload=$((Upload * 8 / 1000 / 1000))
+    Result_Speedtest="$1,$3,$4,$5,\"$Download\",${12},\"$Upload\",${16},$8,$9,${10}"
+
+    DestinationServer="${DestinationServer#?}"
+    DestinationServer="${DestinationServer%?}"
+    Latency="${Latency#?}"
+    Latency="${Latency%?}"
+    PacketLoss="${PacketLoss#?}"
+    PacketLoss="${PacketLoss%?}"
+    URL="${URL#?}"
+    URL="${URL%?}"
 }
 
 GetTimeStamp() {
+    # Generate Date timestamp
     Timestamp=$(date "+%Y-%m-%d %H:%M")
+    # Extract Month from timestamp
     Month=$(date "+%b")
 }
 
 ExportCSV() {
-    Headers="\"Month\",\"Timestamp\",\"Download\",\"Upload\",\"Latency[milliseconds]\",\"Provider\",\"Gateway\",\"Uplink Responsivness\",\"Downlink Responsivness\",\"SpeedTestRAW\""
-    Line="\"$Month\",\"$Timestamp\",\"$Download\",\"$Upload\",\"$Latency\",\"$ISPProvider\",\"$GatewayIP\",\"$UplinkResponsivness\",\"$DownLinkResponsivness\",\"$SpeedTest\""
-
+    Headers="\"Month\",\"Timestamp\",\"ISP\",\"Local Gateway IP\",\"server name\",\"idle latency [ms]\",\"idle jitter [ms]\",\"packet loss [%]\",\"download [Mbps]\",\"download latency [ms]\",\"upload [Mbps]\",\"upload latency [ms]\",\"download bytes\",\"upload bytes\",\"share url\""
+    Line="\"$Month\",\"$Timestamp\",\"$ISPProvider\",\"$GatewayIP\",$Result_Speedtest"
     # Check if the CSV file exists
-    if [ -f "$csv_file" ]; then
+    if [ -f "$OutputFile" ]; then
         # Append the new row to the CSV file
-        echo "$Line" >>"$csv_file"
+        echo "$Line" >>"$OutputFile"
     else
         # Create a new CSV file and add the header row
-        echo "$Headers" >"$csv_file"
-        echo "$Line" >>"$csv_file"
+        echo "$Headers" >"$OutputFile"
+        echo "$Line" >>"$OutputFile"
     fi
+}
+
+DisableConsoleOutput() {
+    exec 3>&2
+    exec 2>/dev/null
+}
+EnableConsoleOutput() {
+    exec 2>&3
 }
 
 Main
